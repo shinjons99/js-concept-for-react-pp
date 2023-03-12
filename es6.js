6cd4e3b01cad415aaf49b1c95261cae7d2d0f9be
shinjons99 <shinjons99@gmail.com>
2023-03-12 23:43:23 +0600

    after half in second

---
+++ es6.js
@@ -0,0 +1,11 @@
+const number = [89, 35, 98, 12];
+
+const student = {
+    name: 'shakib khan',
+    age: 43,
+    movie: ['king khan', 'Dhakar Mastan']
+}
+
+const about = `My Name is ${student.name} age ${student.age} and my movie name are ${student.movie} Number ${number[3]}
+also like movies ${student.movie[0]}`;
+console.log(about);
